8e09adbb7ce5039ce36471106c83fe86e7ea1d06
troy <sabotuer99@gmail.com>
2018-04-03 19:33:55 -0700

    initial commit

---
+++ ascii-art-generator/src/main/java/whorten/ascii_art_generator/App.java
@@ -0,0 +1,13 @@
+package whorten.ascii_art_generator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
